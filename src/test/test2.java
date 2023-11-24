f628105f95ed0630840aa3c8099651d2d9e4369a
JonasRosol <jonas.rosol@infis.cz>
2023-11-24 12:20:11 +0100

    .

---
+++ src/test/test2.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.util.Scanner;
+
+public class test2 {
+
+//    public static void main(String[] args) {
+//        String RESET = "\u001B[0m";
+//        String RED = "\u001B[31m";
+//
+//
+//        int[] pole = new int[20];
+//        int soucet = 0;
+//        for (int i = 0; i < pole.length; i++) {
+//            pole[i] = (int) (Math.random() * 201 - 100);
+//            soucet += pole[i];
+//            System.out.println(pole[i]);
+//        }
+//        System.out.println();
+//        if (soucet <= 0) {
+//            System.out.println(soucet);
+//        } else {
+//            System.out.println(RED + soucet + RESET);
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+
+        int[] pole = new int[10];
+        for (int i = 0; i < pole.length; i++) {
+            pole[i] =
+        }
+    }
+
+}
